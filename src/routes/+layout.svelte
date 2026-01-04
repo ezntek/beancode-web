156dfd2d7cae55070c64f69facd58a4df62b2f1f
ezntek <eason@ezntek.com>
2026-01-05 07:53:51 +0800

    chore: remove useless service worker stuff, revert back to self-hosted pyodide

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,6 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	let { children } = $props();
 	import '../app.css';
-	onMount(() => {
-		if ('serviceWorker' in navigator) {
-			// https://dev.to/stefnotch/enabling-coop-coep-without-touching-the-server-2d3n
-			//
-			navigator.serviceWorker.register('/sw.js').then(
-				function (registration) {
-					console.log('COOP/COEP Service Worker registered', registration.scope);
-					// If the registration is active, but it's not controlling the page
-					if (registration.active && !navigator.serviceWorker.controller) {
-						window.location.reload();
-					}
-				},
-				function (err) {
-					console.log('COOP/COEP Service Worker failed to register', err);
-				}
-			);
-		} else {
-			console.warn('Cannot register a service worker');
-		}
-	});
 </script>
 
 <svelte:head>
